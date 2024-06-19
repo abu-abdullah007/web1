91c18957fb48fa2c41e303d38ccabeae88538f33
abu-abdullah007 <zabernakib12580@gmail.com>
2024-06-19 20:06:50 +0600

    i added side hidden bar javascript code

--- js/home.js
+++ js/home.js
@@ -3,6 +3,13 @@ const hiddenNavBar = document.querySelector('.hidden-navbar')
 const hiddenNavList = document.querySelector('.hidden-nav-list')
 const hideRemoveBtn = document.querySelector('#hide-remove')
 
+const hideRemoveButton = document.querySelector('#slider-active-button')
+const parentSideBar = document.querySelector('.side-slide-box')
+const mainSlider = document.querySelector('.slider-box')
+const hideAddButton = document.querySelectorAll('.card-box h1')
+const cardBoxes = document.querySelectorAll('.card-box')
+const mainSliderDisplay = document.querySelector('.slider-center-lower')
+const body = document.body
 
 navButton.addEventListener("click",()=>{
     hiddenNavBar.classList.add('show-hidden')
@@ -12,4 +19,24 @@ navButton.addEventListener("click",()=>{
 hideRemoveBtn.addEventListener("click",()=>{
     hiddenNavBar.classList.remove('show-hidden')
     hiddenNavList.classList.remove('side-hide')
+})
+
+hideAddButton.forEach((hideAbb,index)=>{
+    hideAbb.addEventListener("click",()=>{
+        parentSideBar.classList.add('side-active')
+        setTimeout(()=>{
+            mainSlider.classList.add('slider-active')
+            body.classList.add('body-active')
+        },10)
+    })
+})
+
+hideRemoveButton.addEventListener("click",()=>{
+    mainSlider.classList.remove('slider-active')
+    mainSlider.classList.add('slide-hidden-active')
+    setTimeout(()=>{
+        mainSlider.classList.remove('slide-hidden-active')
+        parentSideBar.classList.remove('side-active')
+        body.classList.remove('body-active')
+    },300)
 })
